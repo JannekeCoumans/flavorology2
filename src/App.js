9787e1286658f38635779d34bf15da1e90c51ee7
Janneke Coumans <60965883+JannekeCoumans@users.noreply.github.com>
2023-09-29 12:16:55 +0200

    Silenced warning

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import AppContextProvider from "contexts/AppContext";
 import "./assets/styles/styles.scss";
 import { AppRouter } from "config/C4";
 
+// eslint-disable-next-line no-unused-vars
 import firebase from "./FirebaseConfig";
 
 function App() {
